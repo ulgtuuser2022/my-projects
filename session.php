31d777d077a06703e91f99e7817a5ab13a6d4940
tuder666 <zvezda2004@list.ru>
2022-09-13 22:26:47 +0400

    Update session.php

--- session.php
+++ session.php
@@ -17,7 +17,7 @@ $login = $_POST['login'];
 $password = $_POST['password'];
 
 
-if(($logC == null and $passC == null) and $login == $logS and $password == $passS)){
+if(($logC == null and $passC == null) and ($login == $logS and $password == $passS)){
 setcookie("login", $login, time()+3600);
 setcookie("password", $password, time()+3600);
 }
